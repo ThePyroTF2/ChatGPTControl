1fed251b649cab538cf92bce4514491e4a81fa25
Devin Droddy <devin.droddy@gmail.com>
2023-04-26 10:49:01 -0400

    Configs now work to let me import packages

--- SeleniumTest.ts
+++ SeleniumTest.ts
@@ -1,17 +1,40 @@
-import { Builder } from 'selenium-webdriver'
+import { Builder, Key, until } from 'selenium-webdriver'
+import Prompt from 'prompt'
 
-const driver = await new Builder().forBrowser('chrome').build()
-
-const approve = async (code: () => unknown) => {
-	const approved = (await prompt(`Run the following code? (y/n)\n\n${code.toString()}\n`))?.toLowerCase()
+const approve = async (code: () => Promise<void>, fallback: () => Promise<void> = () => new Promise(() => {})) => {
+	const promptString = `Run the following code? (y/n)\n\n${code.toString()}\n`
+	const res = await Prompt.get<{[key: string]: string}>([promptString])
+	const approved = res[promptString].toLowerCase()
 	if(approved == 'y') {
 		console.log()
-		code()
+		try{
+			await code()
+		} catch(e) {
+			await fallback()
+			console.error(e)
+		}
 		return
 	}
 	return
 }
 
-approve(() => {
-	driver.get('https://www.google.com')
-})
+const main = async () => {
+	Prompt.start()
+
+	const driver = await new Builder().forBrowser('chrome').build()
+
+	try{
+		await approve(async () => await driver.get('https://www.google.com'))
+		await approve(async () => {
+			await (await driver.findElement({name: 'q'}))?.sendKeys(`Selenium${Key.RETURN}`)
+			await driver.wait(until.elementLocated({css: '#search'}))
+		})
+		await approve(async () => await (await driver.findElement({css: '#search .g a'}))?.click())
+		await approve(async () => await driver.close())
+	} catch (e) {
+		console.error(e)
+		await driver.close()
+	}
+}
+
+main()
