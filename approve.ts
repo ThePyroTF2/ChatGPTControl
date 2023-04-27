8caa0a4fd957634a693cbc5510217898d4b2d222
Devin Droddy <devin.droddy@gmail.com>
2023-04-27 15:00:04 -0400

    Allow for `approve` to pass arguments into its given function

--- approve.ts
+++ approve.ts
@@ -1,18 +1,18 @@
 import Prompt from 'prompt'
 
-type asyncCode = () => Promise<void>
-type syncCode = () => void
+type asyncCode = (...args) => Promise<void>
+type syncCode = (...args) => void
 
 Prompt.start()
 
-export default async function approve(code: asyncCode | syncCode, fallback: asyncCode | syncCode = () => {}) {
+export default async function approve(code: asyncCode | syncCode, fallback: asyncCode | syncCode = () => {}, ...args) {
 	const promptString = `Run the following code? (y/n)\n\n${code.toString()}\n`
 	const res = await Prompt.get<{[key: string]: string}>([promptString])
 	const approved = res[promptString].toLowerCase()
 	if(approved == 'y') {
 		console.log()
 		try{
-			await code()
+			await code(...args)
 		} catch(e) {
 			await fallback()
 			console.error(e)
