e9742adea7219df65d38234876446dbf488daa1c
Devin Droddy <devin.droddy@gmail.com>
2023-04-27 13:51:18 -0400

    Implement `AsyncFunction` constructor

--- SeleniumTest.ts
+++ SeleniumTest.ts
@@ -1,13 +1,13 @@
 import { Builder, Key, until } from 'selenium-webdriver'
 import approve from './approve'
-import { AsyncFunction } from './AsyncFunction'
+import AsyncFunction from './AsyncFunction'
 
 const main = async () => {
 	const driver = await new Builder().forBrowser('chrome').build()
 
 	try{
-		const getGoogle = AsyncFunction(`await driver.get('https://www.google.com')`)
-		// await approve(async () => await driver.get('https://www.google.com'))
+		const getGoogle = new AsyncFunction(`await driver.get('https://www.google.com')`)
+		await approve(getGoogle)
 		await approve(async () => {
 			await (await driver.findElement({name: 'q'}))?.sendKeys(`Selenium${Key.RETURN}`)
 			await driver.wait(until.elementLocated({css: '#search'}))
