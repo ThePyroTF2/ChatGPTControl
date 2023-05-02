90b7ca86eac8067a9ef9d80dd2df63cead085dbc
Devin Droddy <devin.droddy@gmail.com>
2023-05-02 13:52:15 -0400

    Improve readability of `index`

--- index.ts
+++ index.ts
@@ -14,42 +14,53 @@ const openAI = new OpenAIApi(OpenAIConfig)
 const runGivenCode = async (code: string, driver: WebDriver): Promise<string> => {
 	let output = ''
 	const runnableCode = new AsyncFunction('driver', 'Key', 'until', code)
-	const approved = await approve(runnableCode, (e) => e, ['message'], driver, Key, until)
-	if(approved.approved) output += JSON.stringify(approved.return)
+	const approval = await approve(runnableCode, (e) => e, ['message'], driver, Key, until)
+
+	if(approval.approved) output += JSON.stringify(approval.return)
 	else output += '[Code has been denied.]'
-	if(approved.options.message) output += `\n[User message: "${approved.options.message}"]`
+
+	if(approval.options.message) output += `\n[User message: "${approval.options.message}"]`
+
 	return output
 }
 
 const main = async () => {
 	console.clear()
+
 	const driver = await new Builder().forBrowser('chrome').build()
+
 	const prompt: ChatCompletionRequestMessage[] = JSON.parse(await fs.promises.readFile('./setup.json', 'utf-8'))
-		while(true) {
-			try{
-				const res = await openAI.createChatCompletion({
-					model: 'gpt-3.5-turbo',
-					messages: prompt
-				})
-				console.clear()
-				const reply = res.data.choices[0].message
-				console.log(reply.content)
-				prompt.push(reply)
-				const codeFences = reply.content.match(/^`{3}([\S]+)?\n([\s\S]+)\n`{3}/m)
-				const codeFence = codeFences ? codeFences[0] : ''
-				const code = codeFence.replace(/`{3}.*\n?/g, '')
-				let nTokens: number = 0
-				for(const message of prompt) nTokens += encode(message.content).length
-				while(nTokens > 4000) prompt.splice(1, 1)
-				const result = await runGivenCode(code, driver)
-				console.log(`Result: ${result}`)
-				prompt.push({role: 'user', content: result ? result : 'undefined'})
-			} catch (e) {
-				console.error(e)
-				await driver.close()
-				if(e.message.toLowerCase() == 'canceled') break
-			}
-		}	
+
+	while(true) {
+		try{
+			const res = await openAI.createChatCompletion({
+				model: 'gpt-3.5-turbo',
+				messages: prompt
+			})
+
+			console.clear()
+
+			const reply = res.data.choices[0].message
+			console.log(reply.content)
+			prompt.push(reply)
+
+			const codeFences = reply.content.match(/^`{3}([\S]+)?\n([\s\S]+)\n`{3}/m)
+			const codeFence = codeFences ? codeFences[0] : ''
+			const code = codeFence.replace(/`{3}.*\n?/g, '')
+
+			let nTokens: number = 0
+			for(const message of prompt) nTokens += encode(message.content).length
+			while(nTokens > 4000) prompt.splice(1, 1)
+
+			const result = await runGivenCode(code, driver)
+			console.log(`Result: ${result}`)
+			prompt.push({role: 'user', content: result ? result : 'undefined'})
+		} catch (e) {
+			console.error(e)
+			await driver.close()
+			if(e.message.toLowerCase() == 'canceled') break
+		}
+	}	
 }
 
 main()
